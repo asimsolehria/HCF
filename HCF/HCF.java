e25b7570c4c903331d141e7cb622b1925519047c
Asim Habib <de.asimhabib@gmail.com>
2020-03-05 17:51:18 +0500

    Rewrote input prompt

--- HCF/HCF.java
+++ HCF/HCF.java
@@ -10,7 +10,7 @@ public class HCF {
 
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
-        System.out.println("Input a number");
+        System.out.println("Input two numbers for calculating HCF:");
         int number_1=scan.nextInt();
         int number_2=scan.nextInt();
         
