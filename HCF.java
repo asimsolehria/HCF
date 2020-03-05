133981d4bf6929845976d022cbee9c15797d95c2
Asim Habib <de.asimhabib@gmail.com>
2020-03-05 21:28:47 +0500

    Added while loop to calculate HCF

--- HCF.java
+++ HCF.java
@@ -3,13 +3,26 @@
  *
  * @author Asim
  */
-public class HCF {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class HCF 
+{
+
+    public static void main(String[] args) 
+    {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Input two numbers to calculate HCF :");
+        int number_1,number_2,remainder;
+        number_1=scan.nextInt();
+        number_2=scan.nextInt();
+        remainder=number_1%number_2;
+        while(remainder!=0)
+        {
+            number_1=number_2;
+            number_2=remainder;
+            remainder=number_1%number_2;
+        }
+        System.out.println(number_2);
     }
     
 }
