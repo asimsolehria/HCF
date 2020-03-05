91973174757b26591657b554c4fb57b31a61f36d
Asim Habib <de.asimhabib@gmail.com>
2020-03-05 18:08:35 +0500

    Added while loop for HCF calculation

--- HCF/HCF.java
+++ HCF/HCF.java
@@ -8,12 +8,21 @@ import java.util.Scanner;
 public class HCF {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Scanner scan=new Scanner(System.in);
         System.out.println("Input two numbers for calculating HCF:");
         int number_1=scan.nextInt();
         int number_2=scan.nextInt();
         
+        while (number_1%number_2!=0) 
+        {            
+            int temp;
+            temp=number_1;
+            number_1=number_2;
+            number_2=temp;
+        }
+        System.out.println("HCF of "+number_1+" and "+number_2+" is "+number_2);
         
     }
     
