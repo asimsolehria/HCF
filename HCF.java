827f5df9825cdfc6188003edeb49400977414d9f
Arslan Ahmad <bitf16a018@pucit.edu.pk>
2020-03-08 18:46:53 +0500

    improved logic

--- HCF.java
+++ HCF.java
@@ -1,28 +1,42 @@
+import java.util.*;
 
-/**
- *
- * @author Asim
- */
-import java.util.Scanner;
+public class HCF {
 
-public class HCF 
-{
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
 
-    public static void main(String[] args) 
-    {
-        Scanner scan=new Scanner(System.in);
-        System.out.println("Input two numbers to calculate HCF :");
-        int number_1,number_2,remainder;
-        number_1=scan.nextInt();
-        number_2=scan.nextInt();
-        remainder=number_1%number_2;
-        while(remainder!=0)
-        {
-            number_1=number_2;
-            number_2=remainder;
-            remainder=number_1%number_2;
+        System.out.println("enter count of numbers to be taken HCF of: ");
+        int n = keyboard.nextInt();
+
+        while (n <= 0) {
+            System.out.println("PLEASE ENTER A VALID VALUE");
+            System.out.println("enter count of numbers to be taken HCF of: ");
+            n = keyboard.nextInt();
         }
+
+        List<int> numbers = new ArrayList<>();
+
+        int hcf = 0;
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("enter a number: ");
+            numbers.add(keyboard.nextInt());
+        }
+
+        hcf = calc_hcf(numbers.get(0), numbers.get(1));
+
+        for (int j = 2; j < n; j++) {
+            hcf = calc_hcf(hcf, numbers.get(j));
+        }
+
         System.out.println(number_2);
+        keyboard.close();
     }
-    
+
+    int calc_hcf(int n1, int n2) {
+        if (n2 != 0)
+            return calc_hcf(n2, n1 % n2);
+        else
+            return n1;
+    }
 }
